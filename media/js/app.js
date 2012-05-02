31b31a42cd886843b43f9f7044dc23bd6dd4ab62
Lorenzo Pisani <zeelot3k@gmail.com>
2012-05-01 23:55:51 -0700

    document the new code

--- media/js/app.js
+++ media/js/app.js
@@ -44,16 +44,29 @@ window.App = {
 		}
 	});
 
+	/**
+	 * All page views should extend this one. It automatically handles compiling
+	 * templates and placing the content in the specified layout. It also
+	 * supplies a method `renderData` that allows child views to format data
+	 * objects to be passed in when rendering Mustache templates.
+	 */
 	Lib.PageView = Backbone.View.extend({
 		'el': 'body',
+		// The path name to locate the template inside Templates
 		'templatePath': null,
+		// Compiled version gets stored here
 		'template': null,
+		// The path name to locate the template inside Templates
 		'layoutPath': 'layout/empty',
+		// Compiled version gets stored here
 		'layout': null,
 		'initialize': function () {
+			// If a layoutPath was specified
 			if (this.layoutPath) {
+				// Compile it and store it for later use
 				this.layout = Hogan.compile(Templates[this.layoutPath]);
 			}
+			// Same for templatePath
 			if (this.templatePath) {
 				this.template = Hogan.compile(Templates[this.templatePath]);
 			}
@@ -74,6 +87,7 @@ window.App = {
 	});
 
 	Lib.HomePageView = Lib.PageView.extend({
+		// Specify the templatePath and let the PageView do the rest
 		'templatePath': 'page/home'
 	});
 
