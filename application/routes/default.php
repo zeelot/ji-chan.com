48c3ac271cf0bd0c19d8438fbf42585894941756
Lorenzo Pisani <zeelot3k@gmail.com>
2012-06-10 01:32:54 -0700

    all uris served by same kohana action

--- application/routes/default.php
+++ application/routes/default.php
@@ -4,7 +4,7 @@
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('default', '(<uri>)', array('uri' => '.*'))
 	->defaults(array(
 		'controller' => 'main',
 		'action'     => 'index',
