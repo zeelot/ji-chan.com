e038669d2e8fa3c70687d628132200b09bc2f9f8
Lorenzo Pisani <zeelot3k@gmail.com>
2012-06-09 22:18:21 -0700

    remove unneeded files

--- application/config/modules.php
+++ application/config/modules.php
@@ -2,16 +2,12 @@
 
 return array(
 	'assets'             => MODPATH.'assets',
-	'auth'               => MODPATH.'auth',
-	'database'           => MODPATH.'database',
 	'git'                => MODPATH.'git',
-	'image'              => MODPATH.'image',
 	'kostache'           => MODPATH.'kostache',
 	'media'              => MODPATH.'media',
 	'minion'             => MODPATH.'minion',
 	'minion-repo'        => MODPATH.'minion-tasks-repo',
 	'minion-media'       => MODPATH.'minion-tasks-media',
-	'orm'                => MODPATH.'orm',
 	'userguide'          => MODPATH.'userguide',
 	'yform'              => MODPATH.'yform',
 );
